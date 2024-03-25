1ee73e4272607898bfeb380c7d3adfef0d6b1448
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2024-03-25 12:23:34 +0000

    updated dates for presentation purposes

--- plant-api/src/main/resources/data.sql
+++ plant-api/src/main/resources/data.sql
@@ -1,17 +1,17 @@
 
 insert into plant(id,name,description,watering_frequency) values
-(default,'String of Hearts', 'Indirect light', 15),
+(default,'String of Hearts', 'Indirect light', 14),
 (default, 'Begonia Maculata', 'Indirect light, High humidity', 10),
 (default,'Fiddle Leaf Fig', 'Bright indirect light, high humidity', 8),
 (default,'Spirder Plant', 'Part-shade/Full shade, ', 8);
 
 insert into watering(id,plant_id, watered_on) values
-(default, 1, '2024-01-12'),
-(default, 1, '2024-01-08'),
-(default, 2, '2024-02-08'),
-(default, 2, '2024-02-08'),
-(default, 3, '2023-11-12'),
-(default, 3, '2024-01-08'),
-(default, 3, '2024-01-01'),
-(default, 4, '2024-02-10'),
-(default, 4, '2024-01-12');
+(default, 1, '2024-03-14'),
+(default, 1, '2024-02-20'),
+(default, 2, '2024-03-12'),
+(default, 2, '2024-03-02'),
+(default, 3, '2023-02-12'),
+(default, 3, '2024-02-28'),
+(default, 3, '2024-03-12'),
+(default, 4, '2024-03-20'),
+(default, 4, '2024-02-12');
